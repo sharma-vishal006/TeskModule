ec9f6799b5ba5499ce0e4abddf29d3354c09d929
VISHAL SHARMA <VISHAL SHARMA@DESKTOP-2KJNRQ2>
2020-06-20 11:55:38 +0530

    First commit

---
+++ TaskModule/src/Test/Emp.java
@@ -0,0 +1,48 @@
+package Test;
+
+public class Emp {
+	
+	private int Id;
+	private String Name;
+	private String Password;
+	private String Email;
+	private String Country;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	
+	public String getName() {
+		return Name;
+		
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getCountry() {
+		return Country;
+	}
+	public void setCountry(String country) {
+		Country = country;
+	}
+
+	
+	
+	
+
+}
